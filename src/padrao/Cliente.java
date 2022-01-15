ac79a4574d2df43798d822aff9db54d22e7ab986
Francisco53 <franciscolima2228648@gmail.com>
2022-01-15 09:48:45 -0300

    Expert talk

---
+++ src/padrao/Cliente.java
@@ -0,0 +1,14 @@
+package padrao;
+
+public class Cliente {
+
+    private String nome;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
---
+++ src/padrao/IConta.java
@@ -0,0 +1,12 @@
+package padrao;
+
+public interface IConta {
+
+    void sacar(double valor);
+
+    void depositar(double valor);
+
+    void transferir(double valor, IConta contaDestino);
+
+    void imprimirExtrato();
+}
